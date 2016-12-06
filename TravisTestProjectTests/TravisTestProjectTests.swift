3c398bc5bbb459b3b11f907bb109017b48b63f3a
ogusu <xogusu.takuya@contract.isid.co.jp>
2016-12-06 14:29:40 +0900

    tmp

--- TravisTestProjectTests/TravisTestProjectTests.swift
+++ TravisTestProjectTests/TravisTestProjectTests.swift
@@ -32,6 +32,7 @@ class TravisTestProjectTests: XCTestCase {
         // This is an example of a performance test case.
         self.measureBlock {
             // Put the code you want to measure the time of here.
+            print("fin")
         }
     }
     
