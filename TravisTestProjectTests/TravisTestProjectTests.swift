7cbd7df53b0c847f2df170f0e0fa172f066ef0f8
ogusu <xogusu.takuya@contract.isid.co.jp>
2016-12-06 14:46:43 +0900

    tmp

--- TravisTestProjectTests/TravisTestProjectTests.swift
+++ TravisTestProjectTests/TravisTestProjectTests.swift
@@ -25,14 +25,13 @@ class TravisTestProjectTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         XCTAssert(true, "Pass")
-        print("fin")
+        print("成功しました")
     }
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock {
             // Put the code you want to measure the time of here.
-            print("fin")
         }
     }
     
