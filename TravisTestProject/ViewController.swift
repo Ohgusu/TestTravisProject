48a75b16ad1e1d6cdb819bdc063b0f988628e88b
ogusu <xogusu.takuya@contract.isid.co.jp>
2016-12-06 10:37:36 +0900

    tmp

---
+++ TravisTestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TravisTestProject
+//
+//  Created by 開技須磨男 on 2016/12/06.
+//  Copyright © 2016年 kaigi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
